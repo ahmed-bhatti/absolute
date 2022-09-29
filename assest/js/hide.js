78ddbee97a9050ab7d1f3553823fc6bdc261fc91
ahmed ali <ahmedalirajpootbhatti@gmail.com>
2022-09-29 02:30:52 -0700

    first comment

---
+++ assest/js/hide.js
@@ -0,0 +1,9 @@
+// jQuery(function() {
+//     jQuery(".show").click(function(){
+//         jQuery(".breathe").show();
+//     });
+//     jQuery(".show").click(function(){
+//         jQuery(".breathe").hide();
+//         jQuery(".div"+ $(this).attr('target')).show();
+//     });
+// });
